c74ca71e4b8e24a21a2ebac3f89b421206aea14c
Alex Williams <awbeacon@gmail.com>
2016-08-05 19:28:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,26 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+def print_cell(cellString,_cell)
+    print " #{cellString} "
+    if _cell%3 !=2
+      print "|"
+    end
+  if (_cell%3==2 && _cell!=8)
+    puts ""
+    puts "-----------"
+  end
+end
+
+
+def display_board()
+  board = Array.new(9," ")
+         for i in 0..8
+            print_cell(board[i], i)
+          end
+          puts ""
+
+end
+
+
+#puts "Welcome to Tic Tac Toe!"
+display_board()
