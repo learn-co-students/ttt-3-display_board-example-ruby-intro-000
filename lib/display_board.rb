2dfadd926f1452a0e58970db8cb3ef2295c96621
Starsky Eustache <star.eu88@gmail.com>
2016-08-12 23:00:22 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+#board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board
+  row_1 = ("   |   |   ")
+  border_1= "-----------"
+  puts row_1
+  puts border_1
+  puts row_1
+  puts border_1
+  puts row_1
+end
+
+display_board
