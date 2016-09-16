7cf9a614c4fbc44fd922a11503d59ae344d94a48
Eugene Lim <eugene.lim@yale.edu>
2016-09-16 13:54:58 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+#cells = Array.new(9, "   ")
+
+
+
+
+
+
+def display_board
+  #cell = "   "
+  board = "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+  puts board
+end
