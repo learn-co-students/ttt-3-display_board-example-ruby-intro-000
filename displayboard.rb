1f4514fb30cf51b8e21fecbde8806ad5ed738f81
lillian klasen <lillian.klasen@gmail.com>
2016-09-25 14:05:24 +0000

    Done.

---
+++ displayboard.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/display_board'
+def display_board
+  puts "   " "|" "   " "|" "   "
+  puts "-----------"
+  puts "   " "|" "   " "|" "   "
+  puts "-----------"
+  puts "   " "|" "   " "|" "   "
+end
