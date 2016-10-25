06b337d274cfaf4efb46e3be5fb04802aa54b2f5
Matthew Lee <matthewlee07@gmail.com>
2016-10-25 14:53:46 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+a = ["   ", "   ", "   "].join("|")
+b = ["   ", "   ", "   "].join("|")
+c = ["   ", "   ", "   "].join("|")
+puts a
+puts "-----------"
+puts b
+puts "-----------"
+puts c# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+end
