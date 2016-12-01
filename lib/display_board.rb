534ee2011fdf0f237da6c540f5d4305dd0467776
Caroline Hatwell <caroline.hatwell@gmail.com>
2016-12-01 02:45:21 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  i = 0
+  until i == 2
+    puts "   |   |   "
+    puts "-----------"
+    i+=1
+  end
+  puts "   |   |   "
+end
+
+display_board
