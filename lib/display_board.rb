d1e8f242c3f11b0f0edaef62b7e30cfddd2f2381
j silv <jess.pebble@gmail.com>
2017-02-16 23:05:43 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+$board = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+
+def display_board
+  for row in $board
+    print row + "\n"
+  end
+end
+
+#display_board
+#better practice would be leaving 'board' local and saying def display_board(board) and then calling display_board with the argument board
