db8a8fa05a0e4c961c392f4b323df035448e4c4a
Andres Sanin <withgopro@gmail.com>
2017-07-17 21:40:12 +0000

    Done.

---
+++ display_board.rb
@@ -0,0 +1,10 @@
+def display_board
+  puts "Welcome to Tic Tac Toe"
+"   " "|" "   " "|""   "
+"-----------"
+"   " "|" "   " "|""   "
+"-----------"
+"   " "|" "   " "|""   "
+end
+
+  display_board
