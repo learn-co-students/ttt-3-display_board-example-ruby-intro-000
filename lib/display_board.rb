b92dc83d18ce3d6c678b98831128d212edf77a2a
fei <xcasxforever@outlook.com>
2018-06-10 00:50:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   #code
-  puts "A Tic Tac Toe Board"
   puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
 end
