4cd2785cc333128182a8693ac06b55ad03bb5481
Elena Dinca <elenadinca25@gmail.com>
2019-11-24 12:36:52 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
 def display_board
-  puts "A Tic Tac Toe Board"
-end
+   puts row = ["   " "|" "   " "|" "   "]
+   puts separator = "-----------"
+   puts row
+   puts separator
+   puts row
+ end
 
-X = "1, 3, 5, 7, 9"
-0 = "2, 4, 6, 8"
+puts board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
