725a1d313ec48be674cde0ceeafeef8369e4ada2
rocio montero <monterorocios@gmail.com>
2020-03-10 19:08:05 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,7 @@
-// ♥ irb
-001:0 > def display_board
-002:1 >     puts "A Tic Tac Toe Board"
-003:1 >   end
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
