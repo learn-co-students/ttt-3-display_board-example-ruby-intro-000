bac7d3e241c2bbca9d2a298b4380362be3700c99
HAN OCAL <a.ocal88@gmail.com>
2020-06-02 02:05:03 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,26 +6,4 @@ def display_board
   puts "   |   |   "
 end
 
-puts "Welcome to Tic Tac Toe"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-
-puts "What move would X like to make (1-9)?"
-puts "> 5"
-puts "   |   |   "
-puts "-----------"
-puts "   | X |   "
-puts "-----------"
-puts "   |   |   "
-
-puts "What move would O like to make (1-9)?"
-puts "> 1"
-puts " 0 |   |   "
-puts "-----------"
-puts "   | X |   "
-puts "-----------"
-puts "   |   |   "
-# etc...
+display_board
