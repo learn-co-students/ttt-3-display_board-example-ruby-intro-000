cc6142caa62701ddd4c7f0604df100037fdb28d1
Misel Evtimov <misel.evtimov@gmail.com>
2020-07-30 00:17:44 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,8 @@ def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
-display_board
+
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
