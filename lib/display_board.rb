c334ed6253a879c62f974993edca110149b99ca1
Jaimee Lamphiear <2010jlamphiear@gmail.com>
2020-11-12 01:40:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board
   Welcome to Tic Tac Toe
-    |   |
-  ----------
-    |   |
-  ----------
-    |   |
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
+   puts "-----------"
+   puts "   |   |   "
  end
