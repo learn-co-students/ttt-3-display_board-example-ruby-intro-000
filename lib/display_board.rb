4f9988214a186dbe1afb200bde7198cf98d68ecf
benurgh <benurgh@gmail.com>
2020-12-22 18:58:40 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,17 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
+def display_board(move)
+  puts "   |  |    "
+  puts "----------"
+  puts "   |  |    "
+  puts "----------"
+  puts "   |  |    "
 end
+
+def start_game
+  puts "Welcome to Tic Tac Toe"
+  display_board()
+  puts "What move would x like to make (1-9)?"
+  player_xmove = gets.chomp.to_i
+end
+
+start_game
